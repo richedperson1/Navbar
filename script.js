9c473f368fad0f0d16ba02189c529296bd4acc4d
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-06-04 17:10:45 +0530

    smooth transition of nav bars is completed

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const toggleSwitch = document.querySelector(".toggleSwitch i");
+const toggleSwitch = document.querySelector(".menu-switch i");
 
 const menuCards = document.querySelector("div.menu-cards-details");
 const navBarCards = document.querySelector(".nav-bar");
 
 toggleSwitch.addEventListener("click", () => {
   toggleSwitch.classList.toggle("rotate-180");
-
-  menuCards.classList.toggle("toggleFun4Menu");
+  menuCards.classList.toggle("show-height");
 });
